9559d2ef95c93733c29e9fbe7caec8e9f36aa85a
bebbertc <bebbertc@users.noreply.github.com>
2026-03-25 19:52:43 +0300

    🔧 chore(docker): обновлены порты для сервиса sing-box

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -3,7 +3,7 @@ import { SingBoxStatusControl } from "@/features/sing-box/status";
 export function Header() {
   return (
     <header className="bg-sidebar sticky top-0 z-30 flex h-[52px] items-center justify-end px-2 shadow-sm">
-      <SingBoxStatusControl status="ok" />
+      <SingBoxStatusControl />
     </header>
   );
 }
