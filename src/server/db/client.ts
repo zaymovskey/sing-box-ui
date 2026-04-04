0c2e43a686685ae2824d045f3676b9a2c8b4b061
Sergei Zaimovskii <zaims@mail.broward.edu>
2026-04-04 20:50:24 +0300

    🚀 feat(worker/traffic-monitoring): Подготовка к мониторингу трафика юзеров

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -2,22 +2,21 @@ import fs from "node:fs";
 
 import Database from "better-sqlite3";
 
-import { getServerEnv } from "@/shared/lib/server";
-
 import { runMigrations } from "./migrate";
+
+const dbPath = process.env.SQLITE_DB_PATH ?? "/data/app.db";
+console.log("dbPath:", dbPath);
 
 let db: Database.Database | null = null;
 
 function openDb() {
-  const serverEnv = getServerEnv();
-  db = new Database(serverEnv.SQLITE_DB_PATH);
+  db = new Database(dbPath);
   db.pragma("journal_mode = WAL");
   db.pragma("foreign_keys = ON");
 }
 
 function ensureDb() {
-  const serverEnv = getServerEnv();
-  const dbFileExists = fs.existsSync(serverEnv.SQLITE_DB_PATH);
+  const dbFileExists = fs.existsSync(dbPath);
 
   if (!db) {
     openDb();
--- src/server/worker/jobs/traffic-monitor/job.ts
+++ src/server/worker/jobs/traffic-monitor/job.ts
@@ -1,9 +1,46 @@
+import { getDb } from "@/server/db/client";
+
 import { queryStats } from "./test-v2ray-api";
 
+interface TrafficStats {
+  stat: { name: string; value: string }[];
+}
+
+const sql = String.raw;
+
 export function startTrafficMonitor() {
+  const db = getDb();
+
   setInterval(async () => {
     queryStats()
       .then((res) => {
+        const stats: TrafficStats = res as TrafficStats;
+        for (const statItem of stats.stat) {
+          if (!statItem.name.startsWith("user>>>")) {
+            continue;
+          }
+          // const isUpLink = statItem.name.endsWith(">>>traffic>>>uplink");
+
+          //     if (!isUpLink) {
+          //       db.prepare(
+          //         sql`
+          //   UPDATE security_assets
+          //   SET
+          //     name = ?,
+          //     kind = ?,
+          //     server_name = ?,
+          //     updated_at = ?
+          //   WHERE name = ?
+          // `,
+          //       ).run(
+          //         input.name,
+          //         input.type,
+          //         input.serverName ?? null,
+          //         input.updatedAt,
+          //         input.id,
+          //       );
+          //     }
+        }
         console.log("[traffic] stats:");
         console.dir(res, { depth: null });
       })
