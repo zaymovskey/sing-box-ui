e86ee453762026f25c599b3ca6c86d5d3d13753d
Sergei Zaimovskii <zaims@mail.broward.edu>
2026-04-07 22:28:05 +0300

    🚀 feat(pages metadata): Добавлено имя страницы логина

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -8,7 +8,9 @@ export function Header() {
     <header className="bg-sidebar sticky top-0 z-30 flex h-[52px] items-center justify-between px-3 shadow-sm">
       <div className="bg-card dark:bg-input/30 dark:border-input flex items-center gap-2 rounded-md border px-3 py-1 shadow-xs select-none">
         <Image alt="logo" className="h-6 w-6" src={logoPic} />
-        <span className="text-sm font-semibold tracking-tight">sing-box</span>
+        <span className="text-sm font-semibold tracking-tight">
+          sing-box UI
+        </span>
       </div>
 
       <SingBoxStatusControl />
