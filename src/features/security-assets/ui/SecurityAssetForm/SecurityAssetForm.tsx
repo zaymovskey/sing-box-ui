7535a1d66a776dada05a6026caa17c072fb8a6be
bebbert <bebbertc@gmail.com>
2026-04-12 11:07:20 +0300

    🔧 chore(security asset form): Добавлен rhf-devtools

--- src/features/security-assets/ui/SecurityAssetForm/SecurityAssetForm.tsx
+++ src/features/security-assets/ui/SecurityAssetForm/SecurityAssetForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { DevTool } from "@hookform/devtools";
 import { FormProvider, type UseFormReturn, useWatch } from "react-hook-form";
 
 import {
@@ -103,6 +104,8 @@ export function SecurityAssetForm({
           {watchedType === "reality" && <SecurityAssetFormRealityFields />}
         </section>
       </form>
+
+      <DevTool control={form.control} />
     </FormProvider>
   );
 }
