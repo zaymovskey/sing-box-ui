57707a6108553df5d24e6cb7ce28c14f097dff32
bebbert <bebbertc@gmail.com>
2026-04-12 11:04:29 +0300

    🔧 chore(rhf-devtools): Добавлен пакет rhf-devtools

--- src/features/sing-box/inbounds/ui/InboundForm/InboundForm.tsx
+++ src/features/sing-box/inbounds/ui/InboundForm/InboundForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { DevTool } from "@hookform/devtools";
 import { FormProvider, type UseFormReturn, useWatch } from "react-hook-form";
 
 import { type InboundFormValues } from "@/features/sing-box/config-core";
@@ -86,6 +87,8 @@ export function InboundForm({
           {type === "hysteria2" && <InboundFormHy2Fields />}
         </section>
       </form>
+
+      <DevTool control={form.control} />
     </FormProvider>
   );
 }
